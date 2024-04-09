748bbfe905b9d06da8bf7b08b01e0995dac5b7ff
Joseph Johnston <jmjohn29@ncsu.edu>
2024-04-09 12:53:21 -0400

    removed schema comment for user

--- backend/db.js
+++ backend/db.js
@@ -26,33 +26,4 @@ connectToDB();
 // });
 // const RestaurantModel = mongoose.model('restaurant', restaurantSchema);
 
-
-// const ratingSchema = new mongoose.Schema({
-//     rating_name: { type: String, required: true },
-//     score: { type: int, required: true },
-//     restaurant_id: { type:int, required: true },
-//     rating_id: { type:int, required: true },
-//     comments: { type: String },
-// });
-// const RatingModel = mongoose.model('rating', ratingSchema);
-
-// const userSchema = new mongoose.Schema({
-//     user_name: { type: String, required: true },
-//     full_name: { type: String },
-//     hashed_password: { type: String, required: true },
-//     user_email: { type: String, required: true},
-//     user_id: { type:int, required: true },
-//     accountCreated: { type: Date, required: true },
-// });
-// const UserModel = mongoose.model('user', userSchema);
-
-// const contactSchema = new mongoose.Schema({
-//     contact_name: { type: String, required: true },
-//     contact_email: { type: String, required: true},
-//     contact_phone: { type: int, required: true },
-//     contact_id: { type: int, required: true },
-//     message: { type: String, required: true },
-// });
-// const contactModel = mongoose.model('contact', contactSchema);
-
 // app.use(express.json()); // Middleware for parsing JSON bodies
