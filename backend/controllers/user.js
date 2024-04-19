3348b1f59c7f29b9b9af28b5e671ec35fb475d90
johnstj <johnstonjoey2002@hotmail.com>
2024-04-19 16:17:48 -0400

    not working JWT

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,5 +1,8 @@
 var express = require('express');
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+app.use(express.json());
 
 const userSchema = new mongoose.Schema({
     user_name: { type: String },
@@ -11,23 +14,30 @@ const userSchema = new mongoose.Schema({
 const UserModel = mongoose.model('user', userSchema);
 
 async function storeUser(req, res) {
-    const { username, fullname, email, hashedPassword } = req.body;
-    let newUser = new UserModel({
-        user_name: username,
-        full_name: fullname,
-        hashed_password: hashedPassword,
-        user_email: email,
-        account_created: new Date()
+    const { username, fullname, email, password } = req.body;
+    const hashedPassword = await bcrypt.hashSync(password, 10, async function(err, hash) {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            let newUser = new UserModel({
+                user_name: username,
+                full_name: fullname,
+                hashed_password: hashedPassword,
+                user_email: email,
+                account_created: new Date()
+            });
+        
+            try {
+                const savedUser = await newUser.save();
+        
+                console.log(savedUser);
+            }
+            catch(err) {
+                console.log(err);
+            }
+        }
     });
-
-    try {
-        const savedUser = await newUser.save();
-        res.send(savedUser);
-        console.log(savedUser);
-    }
-    catch(err) {
-        console.log(err);
-    }
 }
 
 module.exports = {
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,6 +10,24 @@ router.get('/', function(req, res, next) {
 /* POST new account */
 router.post('/createUser', function(req, res, next) {
   user.storeRating(req, res);
+  res.send();
 });
+
+router.post('/login', function(req, res, next) {
+  const user = 
+});
+
+function ensureToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+  if(typeof bearerHeader !== 'undefined') {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next(); 
+  }
+  else {
+    res.sendStatus(403);
+  }
+}
 
 module.exports = router;
